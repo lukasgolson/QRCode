8c68f0d3ae38369497cecf0cabb375eff089cefc
Lukas Olson <lukasdragon@live.ca>
2024-10-28 20:59:06 -0700

    Removed old periods parameter from run_training

--- train.py
+++ train.py
@@ -189,7 +189,7 @@ def run_training(epochs=24, headless_epochs=6, batch_size=16,  total_items_per_e
 if __name__ == "__main__":
     epochs = 48
     batch_size = 12
-    run_training(epochs=epochs, periods=epochs // 8, batch_size=batch_size, gradient_accumulation_steps=None,
+    run_training(epochs=epochs, batch_size=batch_size, gradient_accumulation_steps=None,
                  total_items_per_epoch=batch_size * 500)
     print("Training complete.")
 
