91cfd8d73fba4fb79d05f66463fbb80e9fe5d3f8
Lukas Olson <lukasdragon@live.ca>
2024-10-28 23:11:39 -0700

    Fixed SpatialTransformer.py

--- layers/SpatialTransformer.py
+++ layers/SpatialTransformer.py
@@ -107,10 +107,11 @@ class SpatialTransformer(Layer):
 
         # Scale grid from [-1, 1] to image coordinates
 
-        base_term = tf.cast(0.5, self.compute_dtype)
+        width_f = tf.cast(width, self.compute_dtype)
+        height_f = tf.cast(height, self.compute_dtype)
 
-        x = base_term* tf.cast((x + 1.0) * width - 1, self.compute_dtype)
-        y = base_term * tf.cast((y + 1.0) * height - 1, self.compute_dtype)
+        x = 0.5 * tf.cast((x + 1.0) * width_f - 1, self.compute_dtype)
+        y = 0.5 * tf.cast((y + 1.0) * height_f - 1, self.compute_dtype)
 
         # Get the corner pixel values around the transformed coordinates
         x0 = tf.floor(x)
