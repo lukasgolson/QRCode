7858d2dfc3e8e2110528706a77098b695008625d
Lukas Olson <lukasdragon@live.ca>
2024-11-01 19:38:24 -0700

    Added encoder decoder architecturre

--- ImageCleanModel.py
+++ ImageCleanModel.py
@@ -4,12 +4,27 @@ from keras.src import layers
 
 import tensorflow as tf
 from keras import backend as K
+from keras.src.optimizers import Adafactor
 from tensorflow.keras.callbacks import TensorBoard
 
 import Dataset
 from layers.SpatialAttention import SpatialAttention
 from layers.SpatialTransformer import SpatialTransformer
 
+def encoder(x):
+    for filters in [64, 128, 256]:
+        x = layers.Conv2D(filters, 3, activation='relu', padding='same')(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.MaxPooling2D(pool_size=(2, 2))(x)
+    return x
+
+
+def decoder(x):
+    for filters in [256, 128, 64]:
+        x = layers.UpSampling2D(size=(2, 2))(x)
+        x = layers.Conv2D(filters, 3, padding='same', activation='relu')(x)
+        x = layers.BatchNormalization()(x)
+    return x
 
 def create_model(input_shape):
     # Define the input layer
@@ -18,16 +33,18 @@ def create_model(input_shape):
     # Apply spatial transformer to the input image
     x = SpatialTransformer()(inputs)
 
-    # Encoder: A series of convolutional layers to process the noisy input image
-    x = layers.Conv2D(64, 3, activation='relu', padding='same')(x)
+
+    x = encoder(x)
 
     x = SpatialAttention()(x)
 
-    # Final convolution to output a single-channel image, corrected version of input
-    output = layers.Conv2D(1, 1, activation='sigmoid', padding='same')(x)
+
+    x = decoder(x)
+
+
 
     # Define the model
-    model = Model(inputs, output, name='qr_correction_model')
+    model = Model(inputs, x, name='qr_correction_model')
 
     return model
 
@@ -57,13 +74,24 @@ def train_model(resolution=256, epochs=100):
         TensorBoard(log_dir="image_clean", histogram_freq=1, write_graph=True, write_images=False, update_freq='epoch')
     ]
 
+    optimizer = Adafactor(
+        learning_rate=1.0,
+        beta1=0.9,
+        beta2=0.999,
+        scale_parameter=True,
+        relative_step=True
+    )
+
+
     with strategy.scope():
         model = create_model(input_shape)
 
         dataset = Dataset.create_dataset(paired=True, target_size=(resolution, resolution))
 
         # Compile the model
-        model.compile(optimizer='adam', loss=combined_mse_ssim_loss, metrics=[ssim_loss])
+        model.compile(optimizer=optimizer, loss=combined_mse_ssim_loss, metrics=[ssim_loss])
+
+        model.summary()
 
     # Train the model
     model.fit(dataset, epochs=epochs, steps_per_epoch=250, callbacks=callbacks)
