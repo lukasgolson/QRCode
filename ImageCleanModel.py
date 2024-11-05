efae0ac28fd8469cd5354ce0230c034005f565d4
Lukas Olson <lukasdragon@live.ca>
2024-11-04 16:28:56 -0800

    removed metrics

--- ImageCleanModel.py
+++ ImageCleanModel.py
@@ -138,7 +138,7 @@ def train_model(resolution=256, epochs=100, batch_size=64, jit=False):
             jit = "auto"
 
         # Compile the model
-        model.compile(optimizer=optimizer, loss=loss_func, metrics=["mse", mse_loss, binarized_bce_loss, edge_loss],
+        model.compile(optimizer=optimizer, loss=loss_func, metrics=["mse"],
                       jit_compile=jit)
 
         model.summary()
