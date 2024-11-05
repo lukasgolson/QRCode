1ce30948f3fb043e9267a76ee22b75300e4c6113
Lukas Olson <lukasdragon@live.ca>
2024-11-04 19:26:02 -0800

    Added another CNN layer near the end

--- ImageCleanModel.py
+++ ImageCleanModel.py
@@ -54,7 +54,8 @@ def create_model(input_shape):
 
     x = SoftThresholdLayer()(x)
 
-    x = layers.Conv2D(1, 1, activation='relu', padding='same')(x)
+    x = layers.Conv2D(8, 3, activation='relu', padding='same')(x)
+    x = layers.Conv2D(4, 3, activation='relu', padding='same')(x)
 
     output = layers.Conv2D(1, 1, activation='linear', padding='same')(x)
 
