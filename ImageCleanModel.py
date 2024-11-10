fc8eb887d760c5e6f18caa1eec9f852d9819b983
Lukas Olson <lukasdragon@live.ca>
2024-11-10 02:16:20 -0800

    Mixed precision

--- ImageCleanModel.py
+++ ImageCleanModel.py
@@ -22,13 +22,12 @@ from layers.SpatialAttention import SpatialAttention
 from layers.SpatialTransformer import SpatialTransformer
 from layers.SqueezeExcitation import SqueezeExcitation
 
+# Set global precision policy to mixed precision
+tf.keras.mixed_precision.set_global_policy('mixed_float16')
 
 def Conv2DSkip(input_layer, filters, kernel_size, padding='same', coordConv=False):
     # Perform convolution on the input layer
-
-    # Create a skip connection
     skip = input_layer
-
     if skip.shape[-1] != filters:
         skip = layers.Conv2D(filters, kernel_size=(1, 1), padding=padding)(skip)
 
@@ -38,91 +37,61 @@ def Conv2DSkip(input_layer, filters, kernel_size, padding='same', coordConv=Fals
         x = layers.Conv2D(filters, kernel_size, padding=padding)(input_layer)
 
     x = layers.LeakyReLU()(x)
-
     x = SqueezeExcitation()(x)
 
     # Add the skip connection to the output of the convolutional layer
     x = layers.Add()([skip, x])
     x = layers.LeakyReLU()(x)
 
-    # Apply the activation function after the addition
     return x
 
-
 def create_generator(input_shape):
-    # Define the input layer
     inputs = layers.Input(shape=input_shape)
 
     x = inputs
-
     harmonic = HarmonicConv2D(8, 3, num_groups=1)(x)
     localCnn = layers.Conv2D(8, 3, padding='same')(x)
     globalCnn = layers.Conv2D(8, 3, padding='same')(localCnn)
 
     x = Concatenate(axis=-1)([localCnn, globalCnn, harmonic])
-
     x = DeformableConv2D(32, 3, 4)(x)
-
     x = layers.LeakyReLU()(x)
-
-
 
     x = Conv2DSkip(x, 48, 3)
-
-
     x = layers.LeakyReLU()(x)
-
-
     x = Conv2DSkip(x, 48, 3)
 
     x = layers.Conv2D(64, 3, strides=2, padding='same')(x)
     x = layers.LeakyReLU()(x)
 
-    # x = HarmonicConv2D(32, 3)(x)
-
     x = DeformableConv2D(64, 3, 4)(x)
 
-
-    # upscale
     x = layers.Conv2DTranspose(32, 3, strides=2, padding='same')(x)
     x = Conv2DSkip(x, 32, 3)
 
-    output = Conv2D(1, 1, padding='same', activation='sigmoid')(x)
+    # Set final layer to float32 for numerical stability
+    output = Conv2D(1, 1, padding='same', activation='sigmoid', dtype='float32')(x)
 
-    # Define the model
     model = Model(inputs, output, name='qr_correction_model')
-
     return model
-
 
 def create_discriminator(input_shape):
     dirty_inputs = layers.Input(shape=input_shape, name='dirty_input')
     clean_inputs = layers.Input(shape=input_shape, name='clean_input')
 
-    # Concatenate inputs along the channel axis
     inputs = layers.Concatenate(axis=-1)([dirty_inputs, clean_inputs])
-
     deform = DeformableConv2D(8, 3)(inputs)
-
     conv = layers.Conv2D(8, 3, padding='same')(inputs)
 
     x = Concatenate(axis=-1)([conv, deform])
-
     x = CoordConv(16, 3)(x)
-
     x = layers.LeakyReLU()(x)
 
     x = layers.Conv2D(32, 3, strides=2, padding='same')(x)
-
-
     x = layers.LeakyReLU()(x)
-
     x = DeformableConv2D(32, 3, 4)(x)
-
     x = layers.LeakyReLU()(x)
-
     x = layers.Conv2D(64, 3, padding='same')(x)
-
     x = layers.LeakyReLU()(x)
 
     gap = layers.GlobalAveragePooling2D()(x)
@@ -133,29 +102,25 @@ def create_discriminator(input_shape):
     x = layers.LeakyReLU()(x)
     x = layers.Dense(256)(x)
 
-    x = layers.Dense(1)(x)  # Output layer for binary classification
+    # Set output layer to float32 for stability
+    x = layers.Dense(1, dtype='float32')(x)
 
     model = Model(inputs=[dirty_inputs, clean_inputs], outputs=x, name='discriminator_model')
     return model
-
 
 @tf.function
 @keras.saving.register_keras_serializable()
 def mse_loss(y_true, y_pred):
     return tf.reduce_mean(tf.square(y_true - y_pred))
 
-
 @tf.function
 def gradient_penalty(critic, dirty_images, real_images, fake_images):
     batch_size = tf.shape(real_images)[0]
     epsilon = tf.random.uniform([batch_size, 1, 1, 1], 0.0, 1.0)
-
-    # Interpolate between real and fake clean images
     interpolated_clean_images = epsilon * real_images + (1 - epsilon) * fake_images
 
     with tf.GradientTape() as gp_tape:
         gp_tape.watch(interpolated_clean_images)
-        # Critic prediction on interpolated images with dirty images fixed
         pred = critic([dirty_images, interpolated_clean_images], training=True)
 
     grads = gp_tape.gradient(pred, [interpolated_clean_images])[0]
@@ -163,14 +128,10 @@ def gradient_penalty(critic, dirty_images, real_images, fake_images):
     gp = tf.reduce_mean((grad_l2 - 1.0) ** 2)
     return gp
 
-
 def train_gan(generator, discriminator, gen_optimizer, disc_optimizer, dataset, val_dataset, epochs, callbacks,
               log_interval=10, steps_per_epoch=250, steps_per_val=10, disc_steps=3, lambda_l1=0.1, lambda_gp=10.0):
-    # turn callbacks into a list
     if not isinstance(callbacks, list):
         callbacks = [callbacks]
-
-    print("Callbacks: ", callbacks)
 
     callback_list = tf.keras.callbacks.CallbackList(
         callbacks, add_history=True, model=generator)
@@ -187,37 +148,26 @@ def train_gan(generator, discriminator, gen_optimizer, disc_optimizer, dataset, 
     for epoch in range(epochs):
         callback_list.on_epoch_begin(epoch, logs=logs)
 
-        print(f"Epoch {epoch + 1}/{epochs}")
-
-        # Limit the number of steps per epoch if specified
         steps_in_epoch = steps_per_epoch or len(dataset)
         steps_in_val = steps_per_val or len(val_dataset)
 
         disc_step_count = 0
-
         g_loss = tf.constant(-1.0)
 
-        for step, (clean_images, dirty_images) in enumerate(dataset.take(steps_in_epoch)):  # Use .take to limit steps
-            # Generate transformed images
-
+        for step, (clean_images, dirty_images) in enumerate(dataset.take(steps_in_epoch)):
             callback_list.on_batch_begin(step, logs=logs)
             callback_list.on_train_batch_begin(step, logs=logs)
 
             transformed_images = generator(dirty_images, training=True)
 
-            # Train Discriminator manually
+            # Train Discriminator
             with tf.GradientTape() as tape_d:
                 real_pred = discriminator([dirty_images, clean_images], training=True)
-                # Fake pairs (dirty image, generated image)
                 fake_pred = discriminator([dirty_images, transformed_images], training=True)
-
                 identity_loss = sum(discriminator.losses)
-
                 gp = gradient_penalty(discriminator, dirty_images, clean_images, transformed_images)
-
                 d_loss = tf.reduce_mean(fake_pred) - tf.reduce_mean(real_pred) + identity_loss + lambda_gp * gp
 
-            # Get discriminator gradients and apply them
             grads_d = tape_d.gradient(d_loss, discriminator.trainable_variables)
             disc_optimizer.apply_gradients(zip(grads_d, discriminator.trainable_variables))
 
@@ -225,7 +175,6 @@ def train_gan(generator, discriminator, gen_optimizer, disc_optimizer, dataset, 
             if disc_step_count >= disc_steps:
                 disc_step_count = 0
 
-                # Train Generator (via GAN model) manually
                 with tf.GradientTape() as tape_g:
                     generated_images = generator(dirty_images, training=True)
                     gan_output = discriminator([dirty_images, generated_images], training=True)
@@ -233,48 +182,34 @@ def train_gan(generator, discriminator, gen_optimizer, disc_optimizer, dataset, 
                     g_loss = -tf.reduce_mean(gan_output) + lambda_l1 * tf.reduce_mean(
                         tf.abs(clean_images - generated_images)) + identity_loss
 
-                # Get generator gradients and apply them
                 grads_g = tape_g.gradient(g_loss, generator.trainable_variables)
                 gen_optimizer.apply_gradients(zip(grads_g, generator.trainable_variables))
 
-            else:
-                g_loss = g_loss
-
-            # Trigger callbacks
-
             logs = {'d_loss': d_loss, 'g_loss': g_loss}
-
             callback_list.on_train_batch_end(step, logs=logs)
 
             if step % log_interval == 0:
                 print(f"Step: {step + log_interval}/{steps_in_epoch}, D Loss: {d_loss:.4f}, G Loss: {g_loss:.4f}")
 
-        # Validation step
         val_mse = 0
-        val_step = 0  # Initialize the val_step variable
+        val_step = 0
         for val_step, (val_real_images, val_dirty_images) in enumerate(val_dataset.take(steps_in_val)):
             callback_list.on_test_batch_begin(val_step, logs=logs)
-
             val_fake_images = generator(val_dirty_images, training=False)
             val_mse += tf.reduce_mean(tf.square(val_real_images - val_fake_images)).numpy()
-
             callback_list.on_test_batch_end(val_step, logs=logs)
 
-        # Average MSE over all validation steps
-        val_mse /= (val_step + 1)  # `val_step` is the index of the last batch, so +1 for the total number of steps
+        val_mse /= (val_step + 1)
         print(f"Validation MSE after Epoch {epoch + 1}: {val_mse:.4f}")
 
-        # Trigger callbacks
         logs = {'d_loss': d_loss, 'g_loss': g_loss, 'val_mse': val_mse}
         callback_list.on_epoch_end(epoch, logs=logs)
 
     callback_list.on_train_end(logs=logs)
 
-
 def train_model(resolution=256, epochs=100, batch_size=32, jit=False):
     strategy = tf.distribute.MirroredStrategy()
 
-    # with strategy.scope():
     generator = create_generator((resolution, resolution, 1))
 
     if not Path("models").exists():
@@ -282,24 +217,19 @@ def train_model(resolution=256, epochs=100, batch_size=32, jit=False):
         print("Directory 'models' created")
     generator.save("models/qr_correction_model.keras")
 
-    # create models directory if it doesn't exist
-
     discriminator = create_discriminator((resolution, resolution, 1))
 
-    dataset = Dataset.create_dataset(paired=True, target_size=(resolution, resolution), batch_size=batch_size,
-                                     noisiest_epoch=0)
-    val_dataset = Dataset.create_dataset(paired=True, target_size=(resolution, resolution), batch_size=batch_size,
-                                         noisiest_epoch=50,
-                                         batches_per_epoch=1)
+    dataset = Dataset.create_dataset(paired=True, target_size=(resolution, resolution), batch_size=batch_size, noisiest_epoch=0)
+    val_dataset = Dataset.create_dataset(paired=True, target_size=(resolution, resolution), batch_size=batch_size, noisiest_epoch=50, batches_per_epoch=1)
 
     callbacks = [
-        TensorBoard(log_dir="logs/fit/ImageCleanModel/" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"),
-                    profile_batch=0),
+        TensorBoard(log_dir="logs/fit/ImageCleanModel/" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), profile_batch=0),
         RollingAverageModelCheckpoint(filepath='best_model.keras', monitor='val_mse', save_best_only=True, mode='min')
     ]
 
-    gen_optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4, beta_1=0.0, beta_2=0.9)
-    disc_optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4, beta_1=0.0, beta_2=0.9)
+    # Wrap optimizers for mixed precision
+    gen_optimizer = tf.keras.mixed_precision.LossScaleOptimizer(tf.keras.optimizers.Adam(learning_rate=1e-4, beta_1=0.0, beta_2=0.9))
+    disc_optimizer = tf.keras.mixed_precision.LossScaleOptimizer(tf.keras.optimizers.Adam(learning_rate=1e-4, beta_1=0.0, beta_2=0.9))
 
     jit = jit if jit else "auto"
 
@@ -309,13 +239,10 @@ def train_model(resolution=256, epochs=100, batch_size=32, jit=False):
     generator.save('qr_correction_model.keras')
     discriminator.save('discriminator_model.keras')
 
-
 if __name__ == '__main__':
-    # get batch size argument
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch_size", type=int, default=64)
-    parser.add_argument("-JIT", nargs="?", default=False, const=True, type=bool,
-                        help="Enable Just-In-Time compilation.")
+    parser.add_argument("-JIT", nargs="?", default=False, const=True, type=bool, help="Enable Just-In-Time compilation.")
 
     batch_size = parser.parse_args().batch_size
     jit_compile = parser.parse_args().JIT
